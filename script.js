3014daed5b2c12e86248080f6df03764ba4750d2
karina <123593292+Kuksito@users.noreply.github.com>
2024-09-30 15:56:58 +0300

    Add func for displaying books in an array

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector('[data-container]');
-
-const addButton = document.createElement('button');
-addButton.classList.add('btn');
-container.append(addButton);
+const cardContainer = document.querySelector('[data-card-container]');
+const formContainer = document.querySelector('[data-form]');
+const dialog = document.querySelector('[data-dialog]');
+const addBookBtn = document.querySelector('[data-btn]');
+const submitBtn = document.querySelector('[data-submit]');
 
 const myLibrary = [
     {
@@ -10,26 +11,32 @@ const myLibrary = [
         author: 'L. J. Smith',
         published: '1991',
         pages: 272,
+        read: 'have read',
     },
     {
         title: 'The Daughter of Smoke and Bone',
         author: 'Laini Taylor',
         published: '2011',
         pages: 418,
+        read: `didn't read`,
     },
     {
         title: 'The Poppy War',
         author: 'R. F. Kuang',
         published: '2018',
         pages: 544,
+        read: 'read',
     },
     {
         title: 'Babel',
         author: 'R. F. Kuang',
         published: '2022',
         pages: 545,
+        read: 'read',
     },
 ];
+
+// addBookBtn.addEventListener('click', addBookToLibrary);
 
 function Book(title, author, published, pages, notRead){
     this.title = title;
@@ -39,12 +46,36 @@ function Book(title, author, published, pages, notRead){
     this.notRead = notRead;
 };
 
-function addBookToLibrary(books){
-    books.forEach((book) => {
-        const card = document.createElement('div');
-        card.append(book);
-        container.append(card);
-    })
+function addBookToLibrary(){
+    const newBook = new Book();
+    myLibrary.push(newBook);
 };
 
-// addBookToLibrary(myLibrary);
+addBookToLibrary();
+
+function displayBook(books){
+    books.forEach(book => {
+        const card = document.createElement('div');
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-btn');
+        card.classList.add('card');
+        removeBtn.setAttribute('data', 'remove');
+        card.append(book.title, book.author, book.published, book.pages, book.read);
+        card.append(removeBtn)
+        cardContainer.append(card);
+        
+    })
+}
+
+displayBook(myLibrary);
+
+addBookBtn.addEventListener('click', () => {
+    dialog.style.display = 'flex';
+     dialog.showModal();
+});
+
+submitBtn.addEventListener('click', (e) =>{
+    e.preventDefault();
+    dialog.style.display = 'none'; //the info in the form stays 
+    dialog.close();
+})
