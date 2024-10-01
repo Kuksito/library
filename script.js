941fd22b8df7e076c85efd9898f7c0ac62982502
karina <123593292+Kuksito@users.noreply.github.com>
2024-10-01 15:58:11 +0300

    Reset form, add func to remove btn, display only new added cards

--- script.js
+++ script.js
@@ -6,72 +6,92 @@ const addBookBtn = document.querySelector('[data-btn]');
 const submitBtn = document.querySelector('[data-submit]');
 
 const myLibrary = [
-    {
-        title: 'The vampire diaries',
-        author: 'L. J. Smith',
-        published: '1991',
-        pages: 272,
-        read: 'have read',
-    },
-    {
-        title: 'The Daughter of Smoke and Bone',
-        author: 'Laini Taylor',
-        published: '2011',
-        pages: 418,
-        read: `didn't read`,
-    },
-    {
-        title: 'The Poppy War',
-        author: 'R. F. Kuang',
-        published: '2018',
-        pages: 544,
-        read: 'read',
-    },
-    {
-        title: 'Babel',
-        author: 'R. F. Kuang',
-        published: '2022',
-        pages: 545,
-        read: 'read',
-    },
+    // {
+    //     title: 'The vampire diaries',
+    //     author: 'L. J. Smith',
+    //     published: '1991',
+    //     pages: 272,
+    //     read: 'have read',
+    // },
+    // {
+    //     title: 'The Daughter of Smoke and Bone',
+    //     author: 'Laini Taylor',
+    //     published: '2011',
+    //     pages: 418,
+    //     read: `didn't read`,
+    // },
+    // {
+    //     title: 'The Poppy War',
+    //     author: 'R. F. Kuang',
+    //     published: '2018',
+    //     pages: 544,
+    //     read: 'read',
+    // },
+    // {
+    //     title: 'Babel',
+    //     author: 'R. F. Kuang',
+    //     published: '2022',
+    //     pages: 545,
+    //     read: 'read',
+    // },
 ];
 
-function Book(title, author, published, pages, status){
+function Book(title, author, published, pages, bookStatus){
     this.title = title;
     this.author = author;
     this.published = published;
     this.pages = pages;
-    this.status = status;
+    this.bookStatus = bookStatus;
 };
 
 function addBookToLibrary(){
-    submitBtn.addEventListener('click', (e) => {
-        e.preventDefault();
-        let newBook = new Book(title.value, author.value, published.value, pages.value, status.value);
+    formContainer.addEventListener('submit', (e) => {
+        e.preventDefault();        
+        let newBook = new Book(title.value, author.value, published.value, pages.value, document.querySelector('input[name="book_status"]:checked').value);
         myLibrary.push(newBook);
-        console.log(myLibrary);
-        console.log(newBook);
         dialog.close();
-        displayBook(myLibrary)
-    })
-
+        displayBook();
+        formContainer.reset();
+    });
 };
 
 addBookToLibrary();
 
-function displayBook(books){
-    books.forEach(book => {
-        const card = document.createElement('div');
-        const removeBtn = document.createElement('button');
-        removeBtn.classList.add('remove-btn');
-        card.classList.add('card');
-        removeBtn.setAttribute('data', 'remove');
-        card.append(book.title, book.author, book.published, book.pages, book.read);
-        card.append(removeBtn)
-        cardContainer.append(card);
-        
-    })
+function displayBook(){
+    cardContainer.innerHTML = '';
+    for(let i = 0; i < myLibrary.length; i++){
+        displayBookCard(myLibrary[i]);
+    }
 }
+
+function displayBookCard(book){
+    const card = document.createElement('div');
+    const removeBtn = document.createElement('button');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const bookPublished = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const bookStatus = document.createElement('p');
+    card.classList.add('card');
+    removeBtn.classList.add('remove-btn');
+    removeBtn.setAttribute('data', 'remove');
+    bookTitle.classList.add('book-info');
+    bookAuthor.classList.add('book-info');
+    bookPublished.classList.add('book-info');
+    bookPages.classList.add('book-info');
+    bookStatus.classList.add('book-info');
+    bookTitle.textContent = `Title: ${book.title}`;
+    bookAuthor.textContent = `Author: ${book.author}`;
+    bookPublished.textContent = `Published: ${book.published}`;
+    bookPages.textContent = `Pages: ${book.pages}"`;
+    bookStatus.textContent = `Have read: ${book.bookStatus}`;
+    card.append(bookTitle, bookAuthor, bookPublished, bookPages, bookStatus);
+    card.append(removeBtn);
+    cardContainer.append(card); 
+    removeBtn.addEventListener('click', () =>{
+        card.remove();
+    })
+};
 
 addBookBtn.addEventListener('click', () => {
      dialog.showModal();
